50a9ff8c64050a1fb320cc44e2c6a8d517678e8f
VIPUL SHARMA <vipulsnp@gmail.com>
2020-04-26 22:25:37 +0530

    Added word-break and scroll for comments

--- frontend/src/components/ShowComments.js
+++ frontend/src/components/ShowComments.js
@@ -38,7 +38,7 @@ export default class Comments extends Component{
 
        const Comments=this.state.comments.sort(function(a,b) {return a.date.slice(0,10) - b.date.slice(0,10); }).map(comment=>{
         return(
-            <div key={comment._id} style={{textAlign:"left",margin:"0.1rem",padding:" 0 1rem"}}>
+            <div key={comment._id} style={{textAlign:"left",margin:"0.1rem",padding:" 0 1rem",wordBreak:"break-all"}}>
                 <p><em>{comment.author}</em> : {comment.description} </p>
                 <hr />
             </div>
@@ -51,6 +51,9 @@ export default class Comments extends Component{
                             borderWidth: "5px", 
                             borderImageSource:"linear-gradient(45deg, rgb(0,143,104), rgb(250,224,66))",
                             borderImageSlice:"1",
+                            overflow:"scroll",
+                            overflowX:"hidden",
+                            maxHeight:"210px",
                         }}
             >
                 {Comments}
